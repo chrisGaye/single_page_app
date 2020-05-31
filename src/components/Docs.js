176feaee10274ed235166b165f86d62edd171d6c
chrisGaye <gayetalib90@gmail.com>
2020-05-31 16:03:26 +0000

    BrowserRouter

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -4,6 +4,16 @@ const Docs = () => {
     return (
         <div className="container">
             <h1>Getting started</h1>
+            <div className="row">
+                <div className="col-sm-6">
+                  <span>Nous sommes en train de </span>
+                </div>
+                <div className="col-sm-6">
+                  <span>Expérience fait la différence</span>
+                </div>
+                
+            </div>
+    
         </div>
     )
 }
